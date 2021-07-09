48b004fb55cc96c4281a753a07044b5cad83b177
Gabriel-Ayala <gabrielnevesayala@gmail.com>
2021-07-09 05:30:03 -0300

    ajustes na model e na migration

--- Migrations/20210709075513_createDB.Designer.cs
+++ Migrations/20210709075513_createDB.Designer.cs
@@ -4,14 +4,16 @@ using CadEscola.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CadEscola.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210709075513_createDB")]
+    partial class createDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -12,17 +12,21 @@ namespace CadEscola.Models
     {
         [Key]
         public int AlunoId { get; set; }
-
+        [Required(ErrorMessage = " O campo {0} é obrigatório")]
         [MinLength(3, ErrorMessage = "O {0} não pode conter menos que 3 caracteres.")]
         [MaxLength(200, ErrorMessage = "O {0} não pode conter mais que 200 caracteres.")]
         [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage = "Números e caracteres especiais não são permitidos no nome.")]
         public String Nome { get; set; }
+
+        [Required(ErrorMessage = " O campo {0} é obrigatório")]
         public DateTime DataNascimento { get; set; }
 
         public int NumeroCertidaoNova { get; set; }
 
-        [Index(IsUnique = true)]
+        [StringLength(11)]
+        [RegularExpression(@"/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/", ErrorMessage = "O {0} está invalido" )]
         public String CPF { get; set; }
+        
         public int responsavelId { get; set; }
 
         private class ErrorMessageAttribute : Attribute
