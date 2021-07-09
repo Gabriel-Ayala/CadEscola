7ac1629b240641a3bd2ca67c96ebcec18b452f4f
Gabriel-Ayala <gabrielnevesayala@gmail.com>
2021-07-09 00:47:29 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CadEscola.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
