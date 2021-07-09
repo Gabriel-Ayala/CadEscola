30558e60b2483e46cbbf98efae21327311bb1788
Gabriel-Ayala <gabrielnevesayala@gmail.com>
2021-07-09 01:31:20 -0300

    Criação da Model Aluno e definição dos Atributos

---
+++ Models/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadEscola.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public int NumeroCertidaoNova { get; set; }
+
+        public String CPF { get; set; }
+
+        public int responsavelId { get; set; }
+    }
+}
